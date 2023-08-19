1231bb85c9f4bea53943194383c7043de440b480
GerasHarutyunyan18 <harutyunyan18geras@gmail.com>
2023-08-19 15:11:05 +0400

    changed api url

--- next-app/service/axiosService.tsx
+++ next-app/service/axiosService.tsx
@@ -1,7 +1,7 @@
 import axios, { AxiosInstance, AxiosRequestConfig } from "axios";
 
 export const axiosInstance: AxiosInstance = axios.create({
-  baseURL: "https://b3be-46-71-246-220.ngrok-free.app/",
+  baseURL: "https://3d98-46-71-246-220.ngrok-free.app/",
 });
 
 axiosInstance.defaults.headers.common["Content-Type"] = "application/json";
